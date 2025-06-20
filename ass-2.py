4b20811a7c1b95af4e5032f6fb3734a35f21cf97
prabhudevchandra <chandraprabhudevk@hotmail.com>
2025-06-20 09:45:05 +0530

    just push

---
+++ ass-2.py
@@ -0,0 +1,17 @@
+#Task 1: Check if a Number is Even or Odd
+
+userInput = int(input("Enter a number: "))
+
+if userInput %2 ==0:
+    print(f"{userInput} is an even number.")
+else:
+    print(f"{userInput} is an odd number.")
+
+
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+sum = 0
+
+for i in range(1,51):
+    sum += i
+print(f"The sum of numbers from 1 to 50 is : {sum}")
+
